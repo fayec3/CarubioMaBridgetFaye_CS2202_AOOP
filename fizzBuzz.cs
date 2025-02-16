354cb91beb0c5aaaf052ce6bda8e0d4ef6243fd3
fayec3 <24-09957@g.batstate-u.edu.ph>
2025-02-16 15:29:18 +0800

    Update fizzBuzz.cs

--- fizzBuzz.cs
+++ fizzBuzz.cs
@@ -1,1 +1,29 @@
+using System;
 
+class Fizzbuzz
+{
+    static void Main(string[] args)
+    {
+        int upperBound = 15;
+        
+        for (int num = 1; num <= upperBound; num++)
+        {
+            if (num % 3 == 0 && num % 5 == 0)
+            {
+                Console.WriteLine("FizzBuzz");
+            }
+            else if (num % 3 == 0)
+            {
+                Console.WriteLine("Fizz");
+            }
+            else if (num % 5 == 0)
+            {
+                Console.WriteLine("Buzz");
+            }
+            else
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
